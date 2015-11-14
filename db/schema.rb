aeea00f5d9db8668919e1ab417104e08230f621f
Lays Novaes <lays_novaes@hotmail.com>
2015-11-14 10:52:40 -0200

    criada classe genre

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114124737) do
+ActiveRecord::Schema.define(version: 20151114125145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
