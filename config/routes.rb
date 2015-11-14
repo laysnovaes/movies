3593097289bf82fd8aca5bfca00c932c99cac0ee
Lays Novaes <lays_novaes@hotmail.com>
2015-11-14 13:15:25 -0200

    mudancas graficas

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :movies
   resources :genres
   mount Upmin::Engine => '/admin'
-  root to: 'visitors#index'
+  root to: 'movies#index'
   devise_for :users
   resources :users
 end
